4dfe727f66410347a2e0ef6652f560632a9018e2
koseony <kwakhoseon@gmail.com>
2022-07-19 22:10:43 +0900

    feat: 회원가입 폼

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,8 +10,8 @@ import {
   Row,
   Select,
 } from "antd";
+import { LockOutlined, MailOutlined, UserOutlined } from "@ant-design/icons";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 
 const Register = (props) => {
   const { open, close } = props;
@@ -24,6 +24,10 @@ const Register = (props) => {
     setShowRegis(false);
   };
 
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
   return (
     <div className={open ? "register_wrap open" : "register_wrap"}>
       {open ? (
@@ -32,6 +36,67 @@ const Register = (props) => {
             <button className="closeBtn" onClick={close}>
               &times;
             </button>
+            <div className="main">
+              <h3>회원가입</h3>
+              <Form
+                name="normal_register"
+                className="register-form"
+                onFinish={onFinish}
+              >
+                <Form.Item
+                  name="name"
+                  rules={[
+                    {
+                      type: "name",
+                      message: "이름을 제대로 입력해주세요!",
+                    },
+                    {
+                      required: true,
+                      message: "Please Input your Username",
+                    },
+                  ]}
+                >
+                  <Input
+                    prefix={<UserOutlined className="site-form-item-icon" />}
+                    placeholder="Name"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="email"
+                  rules={[
+                    {
+                      type: "email",
+                      message: "이메일을 제대로 입력해주세요!",
+                    },
+                    {
+                      required: true,
+                      message: "이메일을 입력해주세요!",
+                    },
+                  ]}
+                >
+                  <Input
+                    prefix={<MailOutlined />}
+                    type="email"
+                    placeholder="E-mail"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "비밀번호를 입력해주세요!",
+                    },
+                  ]}
+                >
+                  <Input
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                  />
+                </Form.Item>
+              </Form>
+            </div>
           </div>
         </div>
       ) : null}
